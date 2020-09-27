d41c93f48d09e35be9f0871751ab1e90d670c3bd
Earl Cameron <mr.e.cameron@gmail.com>
2020-09-26 23:32:54 -0400

    uuid

--- backend/Server.js
+++ backend/Server.js
@@ -8,7 +8,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,"
+    "Origin, X-Requested-With, Content-Type, Accept,uuid"
   );
   next();
 });
